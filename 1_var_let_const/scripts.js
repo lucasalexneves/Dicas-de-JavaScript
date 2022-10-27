884796c1ba9d60156046f90f89f5874881543674
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-10-26 22:03:17 -0300

    Já o let separa o valor da variável por bloco, mantendo assim a hierarquia de cada um deles, não havendo sobreposição do valor.

--- 1_var_let_const/scripts.js
+++ 1_var_let_const/scripts.js
@@ -1,9 +1,20 @@
-var nome = "Pedro"
+let nome = "Pedro"
 
 function dizerNome() {
     var nome = "João"
     console.log("O nome é " + nome)
 }
 
+if(true) {
+    let nome = "Salete"
+    console.log(nome)
+}
+
 console.log(nome)
 dizerNome()
+
+for (let i = 0; index < 3; index++) {
+    let nome = "Maria"
+    console.log(nome)
+    
+}
